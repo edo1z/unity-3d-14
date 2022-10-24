d9e8697d00c617f3627bacc5957e01a3f2be84cd
web3ten0 <web3ten0@gmail.com>
2022-10-24 22:22:20 +0900

    Chinemachine修正(3rdPersonFollow+DoNothing)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
     private float _look_sensitive_x = 0.1f;
     private float _look_sensitive_y = 0.05f;
 
-    private GameObject _mesh, _cam, _camParent;
+    private GameObject _camTarget;
     private PlayerInputHandler _input;
     private CharacterController _characon;
 
@@ -14,9 +14,7 @@ public class Player : MonoBehaviour
     {
         TryGetComponent(out _input);
         TryGetComponent(out _characon);
-        _mesh = transform.Find("Mesh").gameObject;
-        _camParent = transform.Find("CameraParent").gameObject;
-        _cam = _camParent.transform.Find("Main Camera").gameObject;
+        _camTarget = transform.Find("CameraTarget").gameObject;
     }
 
     private void Aim()
@@ -25,9 +23,8 @@ public class Player : MonoBehaviour
         if (direction == Vector2.zero) return;
         float x = direction.x * _look_sensitive_x;
         float y = direction.y * _look_sensitive_y * -1;
-        _camParent.transform.Rotate(0, x, 0, Space.World);
-        _camParent.transform.Rotate(y, 0, 0, Space.Self);
-        _mesh.transform.rotation = Quaternion.Euler(0, _camParent.transform.eulerAngles.y, 0);
+        transform.Rotate(0, x, 0, Space.World);
+        _camTarget.transform.Rotate(y, 0, 0, Space.Self);
     }
 
     private void Move()
@@ -35,8 +32,7 @@ public class Player : MonoBehaviour
         Vector2 direction = _input.GetMove();
         if (direction == Vector2.zero) return;
         float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-        targetAngle += _camParent.transform.eulerAngles.y;
-        Vector3 targetDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+        Vector3 targetDirection = Quaternion.Euler(0, targetAngle, 0) * transform.forward;
         _characon.Move(targetDirection * _speed * Time.deltaTime);
     }
 
