77b93d385ed72037275696173790a97f2a5bc663
web3ten0 <web3ten0@gmail.com>
2022-10-24 11:33:30 +0900

    プレイヤーとカメラがマウスの方向を向くようにした

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,28 +2,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 5f;
+    private float _speed = 5f;
+    private float _look_sensitive_x = 0.1f;
+    private float _look_sensitive_y = 0.05f;
 
+    private GameObject _mesh, _cam, _camParent;
     private PlayerInputHandler _input;
-    private GameObject _cam;
     private CharacterController _characon;
 
     private void Awake()
     {
         TryGetComponent(out _input);
         TryGetComponent(out _characon);
-        _cam = GameObject.Find("Main Camera");
+        _mesh = transform.Find("Mesh").gameObject;
+        _camParent = transform.Find("CameraParent").gameObject;
+        _cam = _camParent.transform.Find("Main Camera").gameObject;
+    }
+
+    private void Aim()
+    {
+        Vector2 direction = _input.GetLook();
+        if (direction == Vector2.zero) return;
+        float x = direction.x * _look_sensitive_x;
+        float y = direction.y * _look_sensitive_y * -1;
+        _camParent.transform.Rotate(0, x, 0, Space.World);
+        _camParent.transform.Rotate(y, 0, 0, Space.Self);
+        _mesh.transform.rotation = _camParent.transform.rotation;
     }
 
     private void Move()
     {
         Vector2 direction = _input.GetMove();
-        Vector3 move = new Vector3(direction.x, 0, direction.y);
-        _characon.Move(move.normalized * speed * Time.deltaTime);
+        if (direction == Vector2.zero) return;
+        float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        targetAngle += _camParent.transform.eulerAngles.y;
+        Vector3 targetDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+        _characon.Move(targetDirection * _speed * Time.deltaTime);
     }
 
     private void Update()
     {
+        Aim();
         Move();
     }
 }
