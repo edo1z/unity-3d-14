376a2d9b644752db813985c3b2884bebcc8d540d
web3ten0 <web3ten0@gmail.com>
2022-10-29 14:37:23 +0900

    しゃがんでいるときのPlayerの高さを調整

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // スピード関連
     private float _speed = 5f;
     private float _run_speed_rate = 2f;
     private float _crouch_speed_rate = 0.5f;
     private float _look_sensitive_x = 0.1f;
     private float _look_sensitive_y = 0.05f;
 
+    // Playerの高さ・半径
+    private float _player_height = 1.9f;
+    private float _player_radius = 0.35f;
+    private float _player_crouch_height = 1.3f;
+    private float _player_crouch_radius = 0.55f;
+
+    // Object
     private GameObject _camTarget;
     private PlayerInputHandler _input;
     private CharacterController _characon;
     private Animator _animator;
+
+    // State
+    private bool isCrouching = false;
 
     private void Awake()
     {
@@ -59,13 +70,34 @@ public class Player : MonoBehaviour
 
         _animator.SetFloat("MoveForward", direction.y);
         _animator.SetFloat("MoveRight", direction.x);
-        _animator.SetBool("IsCrouching", _input.GetCrouch());
         _animator.SetBool("IsGrounded", true);
+    }
+
+    private void UpdatePlayerHeight()
+    {
+        if (isCrouching != _input.GetCrouch())
+        {
+            isCrouching = _input.GetCrouch();
+            _animator.SetBool("IsCrouching", isCrouching);
+            if (isCrouching)
+            {
+                _characon.height = _player_crouch_height;
+                _characon.radius = _player_crouch_radius;
+                _characon.center = new Vector3(0, _player_crouch_height / 2f, 0);
+            }
+            else
+            {
+                _characon.height = _player_height;
+                _characon.radius = _player_radius;
+                _characon.center = new Vector3(0, _player_height / 2f, 0);
+            }
+        }
     }
 
     private void Update()
     {
         Aim();
+        UpdatePlayerHeight();
         Move();
     }
 }
